1884f47bec25d8d07c4c917c9de35eb097486e55
Mednikov Yuriy <mednikov.yuriy@gmail.com>
2021-11-23 19:57:51 +0300

    ~ autodelegator for public testnet

--- delegate.py
+++ delegate.py
@@ -1,28 +1,50 @@
 # coding: utf-8
-""" 
-Example:
-root@vmi658412:~# python3 delegate.py
-Enter keyring passphrase:
-umeed tx staking delegate umeevaloper1p25t9pydn2jr5f2gtncwruyen58edaqzkt6un3 3605uumee --gas auto --chain-id umee-betanet-2 --from umee1pppqel2znqzazd5rfy52hcfaawlft64xfq9gpc --fees 1000uumee
-umeed tx staking delegate umeevaloper1p25t9pydn2jr5f2gtncwruyen58edaqzkt6un3 5524357uumee --gas auto --chain-id umee-betanet-2 --from umee1cdq329j2sqghr7fw6ew6qj39yw8hp68xcwa7zw --fees 1000uumee
-root@vmi658412:~#
-
-execute every delegate command manually
-"""
-
 import os
 import json
 from subprocess import check_output
+from subprocess import (PIPE, Popen, CalledProcessError)
 
 
-HOLD_UUMEE = 5000
-COMM_UUMEE = 1000
-VALOPER = "umeevaloper1p25t9pydn2jr5f2gtncwruyen58edaqzkt6un3"
+HOLD_UUMEE = 1
+COMM_UUMEE = 0
+VALOPER = "VALOPER_HERE"
 UMEE_CHAIN = os.environ.get("UMEE_CHAIN")
 
 
+PASSWD = b"PASSWORD_HERE\n"
+
+
+def check_output_input(*popenargs, **kwargs):
+    if 'stdout' in kwargs:
+        raise ValueError('stdout argument not allowed, it will be overridden.')
+    if 'input' in kwargs:
+        if 'stdin' in kwargs:
+            raise ValueError('stdin and input arguments may not both be used.')
+        inputdata = kwargs['input']
+        del kwargs['input']
+        kwargs['stdin'] = PIPE
+    else:
+        inputdata = None
+    process = Popen(*popenargs, stdout=PIPE, **kwargs)
+    try:
+        output, unused_err = process.communicate(inputdata)
+    except:
+        process.kill()
+        process.wait()
+        raise
+    retcode = process.poll()
+    if retcode:
+        cmd = kwargs.get("args")
+        if cmd is None:
+            cmd = popenargs[0]
+        raise CalledProcessError(retcode, cmd, output=output)
+    return output
+
+
+
 def get_accounts():
-    output = check_output(["umeed", "keys", "list"]).decode()
+    output = check_output_input(["umeed", "keys", "list"], input=PASSWD).decode()
+
     lines = output.split("\n")
     accounts = list()
 
@@ -31,43 +53,51 @@ def get_accounts():
         if not line.startswith("address:"):
             continue
         accounts.append(line.split(":")[1].strip())
+
     return accounts
 
 
-def print_delegate(from_account, amount):
-    output = " ".join([
-        "umeed", "tx", "staking", "delegate",
-        VALOPER,
-        "%duumee" % amount,
-        "--gas", "auto",
-        "--chain-id", UMEE_CHAIN,
-        "--from", from_account,
-        "--fees", "%duumee" % COMM_UUMEE
-    ])
+def delegate(from_account, amount):
+    output = " ".join(["umeed", "tx", "staking", "delegate", VALOPER,
+                       "%duumee" % amount,
+                       "--gas", "auto",
+                       "--chain-id", UMEE_CHAIN, "--from", from_account,
+                       "--fees",
+                       "%duumee" % COMM_UUMEE, "-y"])
+
+    command = ["umeed", "tx", "staking", "delegate", VALOPER,
+               "%duumee" % amount,
+               "--gas", "auto",
+               "--chain-id", UMEE_CHAIN,
+               "--from", from_account,
+               "--fees", "%duumee" % COMM_UUMEE,
+               "-y"]
+
+    output = check_output_input(command, input=PASSWD)
     print(output)
 
 
 def get_balance(account):
-    output = check_output([
-        "umeed", "q", "bank", "balances", account, "-o json"]).decode()
+    output = check_output_input([
+        "umeed", "q", "bank", "balances", account, "-o json"], input=PASSWD
+    ).decode()
     balances = json.loads(output)["balances"]
 
     try:
         assert balances[0]["denom"] == "uumee"
-    except (IndexError, KeyError, AssertionError):
-        # print("Unable to get balance for %s: %s" % (account, balances))
+    except (IndexError, KeyError):
         return 0
 
     return int(balances[0]["amount"])
 
 
-def __main__():
-    for account in get_accounts():
-        balance = get_balance(account)
-        amount = balance - HOLD_UUMEE - COMM_UUMEE
-        if amount > 0:
-            print_delegate(account, amount)
+for account in get_accounts():
+    balance = get_balance(account)
 
+    amount = balance - HOLD_UUMEE - COMM_UUMEE
 
-if __name__ == "__main__":
-    __main__()
+    if amount < 0:
+        continue
+
+    #print("Delegate %d uumee from %s" % (amount, account))
+    delegate(account, amount)
